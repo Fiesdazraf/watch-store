a7ce83fded8b577aed8675e126536d86882170af
fiesdazraf <8leorave@gmail.com>
2025-09-07 05:52:00 +0330

    chore(settings): adjust base settings (dj-database-url, etc.)

--- config/settings/base.py
+++ config/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "apps.orders",
     "apps.accounts",
     "apps.payments",
+    "apps.core",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
